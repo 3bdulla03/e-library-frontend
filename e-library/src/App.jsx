94ac3192adfff9f35286e13d0b194d00ab8ac089
Ali19Shamlooh <3looy19shamlooh@gmail.com>
2025-12-22 19:40:54 +0300

    auth setup

--- e-library/src/App.jsx
+++ e-library/src/App.jsx
@@ -1,18 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./App.css"
 
 function App() {
+  const [user, setUser] = useState(null)
 
-
-  return (
-    <>
-    </>
-  )
+  return <></>
 }
 
 export default App
-
-
-
---
+++ e-library/src/pages/Register.jsx
@@ -0,0 +1,17 @@
+import { useState, React } from "react";
+
+
+const Register =()=> {
+
+  const handleChange  = (e)=>{
+    e.preventDefault()
+  }
+
+  return (
+  <>
+    
+  </>
+  )
+}
+
+export default Register
