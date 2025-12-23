c38ea40e528936e50994ffd137b55785d8ef8b97
3bdulla03 <abdulla7545@gmail.com>
2025-12-23 18:53:16 +0300

    setup components

---
+++ e-library/src/components/Book.jsx
@@ -0,0 +1,17 @@
+const Book = ({book}) => {
+
+    return(
+        <>
+        <div className="card book-card" onClick={book.onClick}>
+            <div className="img-wrapper">
+                <img src={book.background_image} alt={book.name} />
+            </div>
+            <div className="info-wrapper flex-col">
+                <h3>{book.name}</h3>
+                <p>{book.author}</p>
+            </div>
+        </div></>
+    )
+}
+
+export default Book
