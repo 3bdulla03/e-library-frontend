929f0cfcbe1e61c36133ed75580d2e86c046296e
3bdulla03 <abdulla7545@gmail.com>
2025-12-23 19:09:13 +0300

    adding routes to main and app

--- e-library/src/App.jsx
+++ e-library/src/App.jsx
@@ -2,11 +2,27 @@ import { useState } from "react"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
 import "./App.css"
+import { Link, Route, Routes } from 'react-router-dom'
 
-function App() {
+import Nav from "./components/Nav"
+import Home from "./pages/Home"
+import Favorites from "./pages/Favorites"
+import BookDetails from "./pages/BookDetails"
+
+const App = () => { 
   const [user, setUser] = useState(null)
 
-  return <></>
+  return (
+    <div>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/details" element={<BookDetails />} />
+      </Routes>
+    </div>
+  )
+ 
 }
 
 export default App
