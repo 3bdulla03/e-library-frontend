3dd3ed518fc78c896f6d577508fe685968ad438f
3bdulla03 <abdulla7545@gmail.com>
2025-12-28 18:12:18 +0300

    style home page

--- e-library/src/components/Nav.jsx
+++ e-library/src/components/Nav.jsx
@@ -6,8 +6,18 @@ const Nav = () => {
         <>
         <header>
             <nav>
-                <Link to="/">Home</Link>
-                <Link to="/Favorites">Favorites</Link>
+                <Link to="/" className="nav-logo-container">
+                    <img 
+                        src="https://i.ibb.co/zHJqB9jP/AAA-library.png" 
+                        alt="AAA Library Logo" 
+                        className="nav-logo-img" 
+                    />
+                <span className="nav-logo-text">AAA Library</span>
+                </Link>
+                <div className="nav-links">
+                    <Link to="/">Home</Link>
+                    <Link to="/Favorites">Favorites</Link>
+                </div>
             </nav>
         </header></>
     )
