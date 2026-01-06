1aaa3e6ad2f2df0d0620e4ab2db9d7daa270f45a
Ali19Shamlooh <3looy19shamlooh@gmail.com>
2026-01-06 19:38:18 +0300

    deploy url

--- e-library/src/services/api.js
+++ e-library/src/services/api.js
@@ -1,6 +1,6 @@
 import Axios from "axios"
 
-export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = "https://render.com/docs/web-services#port-binding"
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
